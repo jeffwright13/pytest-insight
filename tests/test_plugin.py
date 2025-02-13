cd9a877f6ab4af685daa5bc422572be58eef2c6d
Jeff Wrighte <jeff.washcloth@gmail.com>
2025-02-13 13:45:48 -0700

    Added a couple of minor test for plugin.py. Two are commented out atm.

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest_insight import __version__
 
 
@@ -30,3 +31,57 @@ def test_help_shows_insight_option(tester):
     """Verify our plugin option appears in pytest help."""
     result = tester.runpytest("--help")
     result.stdout.fnmatch_lines(["*insight:*", "*--insight*Enable pytest-insight plugin for test history analysis*"])
+
+
+def test_plugin_registers_hooks(tester):
+    """Test that plugin hooks are registered when enabled."""
+    result = tester.runpytest("--insight", "--help")
+    # Plugin should be listed in help output
+    result.stdout.fnmatch_lines(["*Enable the insight plugin*"])
+
+
+# def test_oof_results_hook(tester):
+#     """Test that pytest_oof_results hook receives and processes results."""
+#     # Create a test file with a simple test
+#     tester.makepyfile(
+#         """
+#         def test_example():
+#             assert True
+#         """
+#     )
+
+#     result = tester.runpytest("--insight", "-v")
+#     assert result.ret == 0
+#     # Should show our plugin received results
+#     result.stdout.no_fnmatch_line("*No results received from pytest-oof*")
+
+
+# def test_process_results_called(tester, mocker):
+#     """Test that process_results is called when results are received."""
+#     # Create a test file
+#     tester.makepyfile(
+#         """
+#         def test_example():
+#             assert True
+#         """
+#     )
+
+#     # Run with insight enabled
+#     result = tester.runpytest("--insight", "-v")
+#     assert result.ret == 0
+
+
+@pytest.mark.parametrize("flag", ["--insight", "--oof"])
+def test_plugin_enables_oof(tester, flag):
+    """Test that --insight automatically enables pytest-oof."""
+    tester.makepyfile(
+        """
+        def test_example():
+            assert True
+        """
+    )
+
+    result = tester.runpytest(flag, "-v")
+    assert result.ret == 0
+    # Should see oof plugin in plugins list
+    result.stdout.fnmatch_lines(["*oof-*"])
