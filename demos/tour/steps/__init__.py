34c6ef98c441bf5ec748c727eed93d95cc0fecd0
Jeff Wright <jeff.washcloth@gmail.com>
2025-04-03 18:15:52 -0600

    Cleanup; migrate prints to logging.

--- demos/tour/steps/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Tour steps package."""
-
-from .query_steps import QueryTourSteps
-from .compare_steps import CompareTourSteps
-from .analyze_steps import AnalyzeTourSteps
-
-__all__ = ['QueryTourSteps', 'CompareTourSteps', 'AnalyzeTourSteps']
--- demos/tour/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Tour utilities package."""
-
-from .tour_helpers import TourHelper, Style, TourState
-
-__all__ = ['TourHelper', 'Style', 'TourState']
